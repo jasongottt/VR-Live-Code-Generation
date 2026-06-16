35c4029630d34e2088e43f55793544cd8673b6dd
Jason Gottesman <jgottes@purdue.edu>
2026-06-16 19:21:13 -0400

    added test scene stuff

---
+++ Assets/GeneratedBehaviors/GeneratedBehavior.cs
@@ -0,0 +1,16 @@
+
+using UnityEngine;
+
+public class GeneratedBehavior : MonoBehaviour
+{
+void Start()
+{
+}
+
+void Update()
+{
+    float pulse = 1f + Mathf.Sin(Time.time * 3f) * 0.25f;
+    transform.localScale = new Vector3(pulse, pulse, pulse);
+}
+
+}
