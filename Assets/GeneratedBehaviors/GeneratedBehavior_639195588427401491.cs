7d7b7844466c6e51ca9436876665b212366c557d
Jason Gottesman <jgottes@purdue.edu>
2026-07-13 17:01:34 -0400

    adjusting camera

---
+++ Assets/GeneratedBehaviors/GeneratedBehavior_639195588427401491.cs
@@ -0,0 +1,10 @@
+
+using UnityEngine;
+
+public class GeneratedBehavior_639195588427401491 : MonoBehaviour
+{
+    void Update()
+    {
+        transform.Rotate(Vector3.up * 90f * Time.deltaTime);
+    }
+}
---
+++ Assets/GeneratedBehaviors/GeneratedBehavior_639195588442109501.cs
@@ -0,0 +1,10 @@
+
+using UnityEngine;
+
+public class GeneratedBehavior_639195588442109501 : MonoBehaviour
+{
+    void Update()
+    {
+        transform.Rotate(Vector3.up * 90f * Time.deltaTime);
+    }
+}
